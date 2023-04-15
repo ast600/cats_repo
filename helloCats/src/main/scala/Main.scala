45c1c22c94a01245304288aab93b00e1e0b72fbb
Vasilii Sotnikov <alexisawtooth@gmail.com>
2023-04-15 23:34:42 +0300

    Added code to test cats Show

--- helloCats/src/main/scala/Main.scala
+++ helloCats/src/main/scala/Main.scala
@@ -1,10 +1,21 @@
+import cats.Show
 import cats.Eq
+import cats.syntax.show._
 import cats.syntax.eq._
+import cats.instances.int._
+import cats.instances.string._
 import cats.instances.option._
 
 object Main extends App {
   final case class Cat(name: String, age: Int, color: String)
 
+  implicit val catsShower: Show[Cat] = Show.show[Cat] {kitty =>
+    val name = kitty.name.show
+    val age = kitty.age.show
+    val color = kitty.color.show
+    s"$name is a $age year-old $color cat"
+  }
+  
   implicit val catsEqualizer: Eq[Cat] = {
     Eq.instance[Cat] {(kitty1, kitty2) =>
       (kitty1.name === kitty2.name) &&
@@ -13,14 +24,11 @@ object Main extends App {
     }
   }
 
-  val cat1 = Cat("Chuck", 1, "brown")
-  val cat2 = Cat("Susan", 2, "pinky")
+  val cat = Cat("Chuck", 1, "brown")
 
-  println(cat1 === cat2)
-  println(cat1 =!= cat2)
-
-  val catOption1 = Option(cat1)
+  val catOption1 = Option(cat)
   val catOption2 = Option.empty[Cat]
 
+  println(cat.show)
   println(catOption1 =!= catOption2)
 }
